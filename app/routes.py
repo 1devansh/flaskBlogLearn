c0eb3eb2dce9283b41a64e7b6c36d882cdae74b1
Devansh Sharma <ds8611@bennett.edu.in>
2020-07-23 11:45:38 +0530

    chapter2

--- app/routes.py
+++ app/routes.py
@@ -1,24 +1,18 @@
 from app import appInstance
+from flask import render_template
 
 @appInstance.route('/')
 def index():
   user = {'username': 'Devansh'}
-  return '''
-<html>
-    <head>
-        <title>Home Page - Microblog</title>
-    </head>
-    <body>
-        <h1>Hello, ''' + user['username'] + '''!</h1>
-    </body>
-</html>'''
+  posts =[
+    {
+      'author': {'username': 'John'},
+      'body': 'Beautiful day today'
+    },
+    {
+      'author':{'username': 'Susan'},
+      'body': 'The Avengers movie was so cool'
+    }
+  ]
+  return render_template('index.html', title='Home', user=user, posts = posts)
   
-
-@appInstance.route('/index')
-def indextry():
-  return "This is index page"
-
-
-@appInstance.route('/index.html')
-def indextry2():
-  return " is this index page?"
