b182ae26cef9932238cbd82a2677b3e78f5fe71c
Devansh Sharma <ds8611@bennett.edu.in>
2020-07-24 15:53:59 +0530

    chapter 3-login

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
-
+from config import Config
 from flask import Flask
 
 appInstance = Flask(__name__)
+appInstance.config.from_object(Config)
+
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import appInstance
-from flask import render_template
+from flask import render_template, flash, redirect
+from app.forms import LoginForm
 
 @appInstance.route('/')
 def index():
@@ -16,3 +17,8 @@ def index():
   ]
   return render_template('index.html', title='Home', user=user, posts = posts)
   
+
+@appInstance.route('/login')
+def login():
+  form = LoginForm()
+  return render_template('login.html', title='Sign In', form=form)
